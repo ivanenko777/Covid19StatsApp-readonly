8f28ca24ef943b1f05a339c99543b6f9bf97580a
Vladislav <1019949+ivanevla@users.noreply.github.com>
2021-02-09 20:27:05 +0200

    init

---
+++ src/main/java/net/ivanevla/covid19app/Covid19appApplication.java
@@ -0,0 +1,13 @@
+package net.ivanevla.covid19app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Covid19appApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Covid19appApplication.class, args);
+    }
+
+}
---
+++ src/test/java/net/ivanevla/covid19app/Covid19appApplicationTests.java
@@ -0,0 +1,13 @@
+package net.ivanevla.covid19app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Covid19appApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
