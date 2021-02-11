4f6948d5dc183082ad79491cde2a995928e04400
Vladislav <1019949+ivanevla@users.noreply.github.com>
2021-02-11 14:08:05 +0200

    create CountryDropdown component access CountryDropdown selected_value from App

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import DropdownCountries from "./components/DropdownCountries";
+import React, { useState } from "react";
 
 function App() {
+  const [country_id, setCountryId] = useState("");
+
+  const eventhandler = (data) => {
+    setCountryId(data);
+    // console.log("app: state.selected_value -> " + country_id);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Covid 19 Stats App</h1>
+      <DropdownCountries onChange={eventhandler} />
     </div>
   );
 }
